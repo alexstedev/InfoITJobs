2e29680a63b88fdf3a61f9aaaa8745920dc63d6e
alexstedev <4002tsa@gmail.com>
2022-04-30 12:47:59 +0200

    variat

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewContainerRef } from '@angular/core';
-import { StartModalComponent } from '../start-modal/start-modal.component';
+import { ApplyModalComponent } from '../start-modal/apply-modal.component';
 import { NovoModalService } from 'novo-elements';
 
 
@@ -23,9 +23,12 @@ export class MainPageComponent {
     private modalService: NovoModalService,
     private viewContainerRef: ViewContainerRef,
   ) {
-    this.modalService.open(StartModalComponent, {
-      viewContainer: this.viewContainerRef
-    });
+    // this.modalService.parentViewContainer = this.viewContainerRef;
+    this.modalService.open(ApplyModalComponent, {
+      job: "IT",
+      source: "TEST1",
+      viewContainer: this.viewContainerRef,
+    }).onClosed.then();
   }
 
   public onSidebarFilter(filter: any): void {
