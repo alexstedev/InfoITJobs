3d0eaafd8c40575b45f85ec6eb78d282d895b6da
bibanez <bibanez135@gmail.com>
2022-05-01 02:34:13 +0200

    Api funcional

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -9,8 +9,27 @@ export class HttpService {
 	private url = 'http://127.0.0.1:8000/';
 	constructor(private http: HttpClient) { }
 
-	getTest() {
-		return this.http.get(this.url);
+	getOffers(page?: number, subcategory?: string) {
+		_query = this.url + 'offers?';
+
+		if (typeof subcategory !== "undefined") {
+			if (typeof page !== "undefined") {
+				_query += 'page=' + (page as string) + '&subcategory=' + subcategory;
+			}
+			else {
+				_query += 'subcategory=' + subcategory;
+			}
+		}
+		else {
+			_query += 'page=' + (page as string);
+		}
+
+		return this.http.get(_query);
+	}
+
+	getCategories() {
+		_query = this.url + 'condensed_categories';
+		return this.http.get(_query);
 	}
 
 }
