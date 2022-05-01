b79153b13162610e86eb810da64c255b7cbe70a2
alexstedev <4002tsa@gmail.com>
2022-05-01 02:11:14 +0200

    :O

--- src/app/apiform/apiform.component.ts
+++ src/app/apiform/apiform.component.ts
@@ -1,30 +1,31 @@
 // app.component.ts
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
-	selector: 'app-api-form',
-	templateUrl: './apiform.component.html',
-	styleUrls: ['./apiform.component.scss']
+  selector: 'app-api-form',
+  templateUrl: './apiform.component.html',
+  styleUrls: ['./apiform.component.scss']
 })
-export class APIFormComponent implements OnInit {
-	customerForm: FormGroup;
-	customer: any;
-	message: string;
-	constructor(private formBuilder: FormBuilder) { }
+export class APIFormComponent {
 
-	ngOnInit() {
-		this.customer = {
-			firstName: 'Tina Reactive'
-		};
+  constructor(private router: Router) { }
 
-		this.customerForm = this.formBuilder.group({
-			firstName: [this.customer.firstName, Validators.required]
-		});
-	}
 
-	onSubmit(form: FormGroup) {
-		this.message = 'You typed: ' + form.controls.firstName.value;
-	}
-
+  profileForm = new FormGroup({
+    skill1: new FormControl(false),
+    skill2: new FormControl(false),
+    skill3: new FormControl(false),
+    skill4: new FormControl(false),
+    skill5: new FormControl(false),
+    skill6: new FormControl(false),
+    skill7: new FormControl(false),
+    skill8: new FormControl(false),
+    skill9: new FormControl(false),
+  });
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+    this.router.navigate(['jobs']);
+  }
 }
